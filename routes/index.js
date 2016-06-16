bb2b34034b4ca9078139819e3ccff1d14dd51bb1
anielav <danielavelar@correio.itau.com.br>
2016-06-16 13:10:12 -0300

    Atualização.

--- routes/index.js
+++ routes/index.js
@@ -5,8 +5,8 @@ var port = process.env.PORT || 54000; // used to create, sign, and verify tokens
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('info', {
-        title: 'Hello',
-        message: 'The API is at http://localhost:' + port + '/api'
+        title: 'Hello - Welcome',
+        message: 'Product control API'
     });
 });
 
