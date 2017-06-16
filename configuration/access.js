7e813295f7d630091f1f73548fb0a7695e022b2f
DanielAvelar <daniel.avelar@gmail.com>
2017-06-16 10:08:50 -0300

    Update access.js

--- configuration/access.js
+++ configuration/access.js
@@ -1,7 +1,7 @@
 module.exports = {
-	'secret': 'joaop0705',
+	'secret': '******',
 	'user': 'admin',
-	'password': 'joaop0705*',
-	'databaseUsers': 'mongodb://admin:joaop0705*@ds025232.mlab.com:25232/data-users',
+	'password': '******',
+	'databaseUsers': 'mongodb://admin:******@ds025232.mlab.com:25232/data-users',
 	'databaseApi': 'mongodb://admin:joaop0705*@ds019482.mlab.com:19482/data-api'
 };
