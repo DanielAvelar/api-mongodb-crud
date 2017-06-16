60e8533c6957742f36498ae5c35e2b2a61b34bbf
anielav <danielavelar@correio.itau.com.br>
2017-06-16 10:17:02 -0300

    Update Access.

--- configuration/access.js
+++ configuration/access.js
@@ -1,7 +1,7 @@
 module.exports = {
-	'secret': 'joaop0705',
+	'secret': '3c2181b4-5295-11e7-b114-b2f933d5fe66',
 	'user': 'admin',
-	'password': 'joaop0705*',
-	'databaseUsers': 'mongodb://admin:joaop0705*@ds025232.mlab.com:25232/data-users',
-	'databaseApi': 'mongodb://admin:joaop0705*@ds019482.mlab.com:19482/data-api'
+	'password': 'dataapi*',
+	'databaseUsers': 'mongodb://admin:datausers*@ds025232.mlab.com:25232/data-users',
+	'databaseApi': 'mongodb://admin:dataapi*@ds019482.mlab.com:19482/data-api'
 };
