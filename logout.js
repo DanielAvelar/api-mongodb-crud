f1cc6ece738d11c00b42c3028cd47cf2cca67816
DanielAvelar <daniel.avelar@gmail.com>
2018-11-21 09:42:00 -0200

    teste

--- logout.js
+++ logout.js
@@ -1,9 +1,6 @@
 // Route logout user for system.
 exports.getLogout = function(req, res) {
   req.session['x-access-token'] = null;
-  res.header('Cache-Control', 'private, no-cache, no-store, must-revalidate');
-  res.header('Expires', '-1');
-  res.header('Pragma', 'no-cache');
   res.render('login', {
     title: 'Authenticate'
   });
